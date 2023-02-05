e40bf96e15a37d566a7564997a5f8c39f8162512
studokim <studokim@gmail.com>
2023-02-05 23:46:38 +0300

    add basic lexer

---
+++ lib/lexer.ml
@@ -0,0 +1,115 @@
+(*** Streams ***)
+
+type stream = {
+    index : int;
+    (* before is backwards ordered *)
+    before : char list;
+    next   : char;
+    after  : char list
+}
+
+let read_char stm =
+    match stm.after with
+    | [] -> raise End_of_file
+    | head :: tail -> {
+        index = stm.index + 1;
+        before = stm.next :: stm.before;
+        next = head;
+        after = tail
+    }
+
+let unread_char stm =
+    match stm.before with
+    | [] -> raise End_of_file
+    | head :: tail -> {
+        index = stm.index - 1;
+        before = tail;
+        next = head;
+        after = stm.next :: stm.after
+    }
+
+let stream_of_string str =
+    match Util.explode str with
+    | [] -> raise End_of_file
+    | head :: tail -> {
+            index = 0;
+            before = [];
+            next = head;
+            after = tail
+        }
+
+let syntax_error stm msg =
+    raise (Util.Syntax_error (msg ^ " on character " ^ (string_of_int stm.index)))
+
+(*** by Character ***)
+
+let is_digit c =
+    let code = Char.code c in
+    code >= Char.code('0') && code <= Char.code('9')
+
+let is_alpha c =
+    let code = Char.code c in
+    (code >= Char.code('A') && code <= Char.code('Z')) ||
+    (code >= Char.code('a') && code <= Char.code('z'))
+
+(* stops on the first non-blank *)
+let rec skip_blank_chars stm =
+    let c = stm.next in
+    if c = ' ' || c = '\t' || c = '\r' || c = '\n'
+    then skip_blank_chars (read_char stm)
+    else stm
+
+(*** by Token ***)
+
+(* all lang keywords/symbols *)
+type token =
+    | Identifier of string
+    | Literal of int
+    | Assign
+    | Print
+    | LeftParen | RightParen
+    | AddOp | SubOp | MulOp | DivOp
+
+type scanner = {
+    stream : stream;
+    token : token option
+}
+
+(* scans a stream and returns next token *)
+let scan scanner =
+    let stm = skip_blank_chars scanner.stream in
+    let c = stm.next in
+    let rec scan_identifier stm acc =
+        let stm = read_char stm in
+        let next_c = stm.next in
+        let _ = print_endline ("fine [" ^ Char.escaped next_c ^ "]") in
+            if is_alpha next_c || is_digit next_c || next_c = '_'
+            then scan_identifier stm (acc ^ (Char.escaped next_c))
+            else
+                if acc = "print" then {stream = read_char stm; token = Some Print}
+                else {stream = read_char stm; token = Some (Identifier acc)}
+    in
+    let rec scan_literal stm acc =
+        let stm = read_char stm in
+        let next_c = stm.next in
+            if is_digit next_c
+            then scan_literal stm (acc ^ (Char.escaped next_c))
+            else
+                {stream = read_char stm; token = Some (Literal (int_of_string acc))}
+    in
+    if is_alpha c then scan_identifier stm (Char.escaped c)
+    else if is_digit c then scan_literal stm (Char.escaped c)
+    else match c with
+    | '+' -> {stream = read_char stm; token = Some AddOp}
+    | '-' -> {stream = read_char stm; token = Some SubOp}
+    | '*' -> {stream = read_char stm; token = Some MulOp}
+    | '/' -> {stream = read_char stm; token = Some DivOp}
+    | _ -> syntax_error stm "couldn't identify the token"
+
+(*** Tests ***)
+
+let blank = stream_of_string "   non_blank   "
+let blank_skipped = skip_blank_chars blank
+let program = stream_of_string "x + 1 ---"
+let s = {stream = program; token = None}
+let _ = s
---
+++ lib/util.ml
@@ -0,0 +1,22 @@
+let read_line filepath =
+  let channel = open_in filepath in
+  try
+    let line = input_line channel in
+    close_in channel;
+    line
+  with e -> match e with
+    | End_of_file -> ""
+    | _ -> close_in_noerr channel; raise e
+
+(* Converts a string to a list of chars *)
+let explode str =
+  let rec explode_inner cur_index chars =
+    if cur_index < String.length str then
+      let new_char = str.[cur_index] in
+      explode_inner (cur_index + 1) (chars @ [new_char])
+    else chars in
+  explode_inner 0 []
+
+let eof = '\000'
+
+exception Syntax_error of string
