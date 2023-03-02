aa70e938292195873da4f482fd1aebf1e239c24c
studokim <studokim@gmail.com>
2023-03-02 14:33:41 +0300

    add test .lua programs

---
+++ test/test.0.lua
@@ -0,0 +1,14 @@
+function twice(x)
+    y = (x * 2)
+    return y
+end
+
+function square(a)
+    return (a * a)
+end
+
+title_1 = "square area is"
+a = 5
+area = square(a)
+title_2 = "and its perimeter is"
+perimeter = twice(twice(a))
---
+++ test/test.1.lua
@@ -0,0 +1,12 @@
+function f()
+    x=1
+    --[[comment-y=(1-2comment--]]
+    --[[comment
+    --[[comment
+
+    comment--]]
+    z=3
+end
+;
+
+__show_env()
